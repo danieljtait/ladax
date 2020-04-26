df03d3647cc028f2d48a5da3a44514131be2851f
danieltait <tait.djk@gmail.com>
2020-04-26 15:34:40 +0100

    renamed basic_dgp.py

--- examples/notebooks/inducing_var.py
+++ examples/notebooks/inducing_var.py
@@ -11,6 +11,7 @@ import jax.numpy as jnp
 from flax import nn, optim
 from jax import random
 
+import ladax
 from ladax import kernels
 from ladax.gaussian_processes import inducing_variables
 from ladax import gaussian_processes
@@ -28,8 +29,8 @@ flags.DEFINE_integer(
 
 def get_datasets():
     index_points = onp.random.uniform(size=20).reshape((10, 2))
-
-    return {'index_points': index_points, }
+    y = onp.sin(2*onp.pi*index_points[:, 0]) * onp.cos(onp.pi*index_points[:, 1])
+    return {'index_points': index_points, 'y': y}
 
 
 def main(_):
@@ -41,11 +42,29 @@ def main(_):
         rng, train_ds['index_points'], kernel_fn,
         num_inducing_points=FLAGS.num_inducing_points)
 
-    print(z.locations)
-    fig, ax = plt.subplots()
-    ax.plot(*z.locations.T, '+')
-    plt.show()
+    def create_model(key):
+        inducing_var_kwargs = {'num_inducing_points': FLAGS.num_inducing_points}
 
+        clz = ladax.models.svgp_factory(kernels.RBFKernelProvider,
+                                        inducing_variables.InducingPointsProvider,
+                                        inducing_variable_kwargs=inducing_var_kwargs)
+        _, params = clz.init_by_shape(key, [([10, 2], jnp.float32)], )
+        return nn.Model(clz, params)
+
+    model = create_model(rng)
+    vgp = model(train_ds['index_points'])
+
+    post_gp = vgp.posterior_gp(train_ds['y'], train_ds['index_points'], observation_noise_variance=0., jitter=1e-4)
+    xx = onp.linspace(0., 1., 10)
+    yy = onp.linspace(0., 1., 10)
+    X = onp.column_stack(list(map(onp.ravel, onp.meshgrid(xx, yy))))
+    m = post_gp.mean_function(X)
+
+    m = m.reshape(10, 10)
+
+    fig, ax = plt.subplots()
+    ax.contourf(xx, yy, m)
+    plt.show()
 
 if __name__ == '__main__':
     app.run(main)
