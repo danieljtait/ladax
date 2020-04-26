760cb9fe82b559ebafc76ce74add1fa05e3fec61
danieltait <tait.djk@gmail.com>
2020-04-26 14:43:41 +0100

    factory methods to pre-roll SVGP creation

---
+++ ladax/models/__init__.py
@@ -0,0 +1,1 @@
+from .svgp import SVGPLayer
---
+++ ladax/models/svgp.py
@@ -0,0 +1,23 @@
+import jax.numpy as jnp
+from flax.nn import Module
+from ladax.gaussian_processes import SVGPLayer
+
+
+def svgp_factory(kernel_provider,
+                 inducing_variable_provider,
+                 mean_fn=None,
+                 kernel_fn_kwargs=None,
+                 inducing_variable_kwargs=None):
+
+    mean_fn = mean_fn if mean_fn else lambda x: jnp.zeros(x.shape[-1], x.dtype)
+    kernel_fn_kwargs = {} if kernel_fn_kwargs is None else kernel_fn_kwargs
+    inducing_variable_kwargs = {} if inducing_variable_kwargs is None else inducing_variable_kwargs
+
+    class SVGP(Module):
+        def apply(self, x):
+            kernel_fn = kernel_provider(x, **kernel_fn_kwargs)
+            inducing_var = inducing_variable_provider(x, kernel_fn, **inducing_variable_kwargs)
+            vgp = SVGPLayer(x, mean_fn, kernel_fn, inducing_var)
+            return vgp
+
+    return SVGP
