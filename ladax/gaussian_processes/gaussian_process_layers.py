e920d77beab5c03493dcbb7930f9199469e22bb2
danieltait <tait.djk@gmail.com>
2020-11-27 23:02:07 +0000

    updated SVGPLayer to the linen API

--- ladax/gaussian_processes/gaussian_process_layers.py
+++ ladax/gaussian_processes/gaussian_process_layers.py
@@ -1,19 +1,21 @@
 import jax.numpy as jnp
 import jax.scipy as jscipy
 
-from flax import nn
+from flax import linen as nn
 from ladax import kernels, utils
-
+from typing import Callable
 from .gaussian_processes import VariationalGaussianProcess
+from .inducing_variables import InducingPointsVariable
 
 
 class SVGPLayer(nn.Module):
-    def apply(self,
-              index_points,
-              mean_fn,
-              kernel_fn,
-              inducing_var,
-              jitter=1e-4):
+    mean_fn: Callable
+    kernel_fn: Callable
+    inducing_variables: InducingPointsVariable
+    jitter: float = 1e-4
+
+    @nn.compact
+    def __call__(self,index_points):
         """
         Args:
             index_points: the nd-array of index points of the GP model.
@@ -25,31 +27,33 @@ class SVGPLayer(nn.Module):
         Returns:
             svgp: A sparse Variational GP model.
         """
-        z = inducing_var.locations
-        qu = inducing_var.variational_distribution
+        z = self.inducing_variables.locations
+        qu = self.inducing_variables.variational_distribution
         qu_mean = qu.mean
         qu_scale = qu.scale
 
         # cholesky of the base kernel function applied at the inducing point
         # locations.
         kzz_chol = jnp.linalg.cholesky(
-            utils.diag_shift(kernel_fn(z, z), jitter))
+            utils.diag_shift(self.kernel_fn(z, z), self.jitter))
 
-        if inducing_var.whiten:
+        if self.inducing_variables.whiten:
             qu_mean = kzz_chol @ qu_mean
             qu_scale = kzz_chol @ qu_scale
 
-        z = inducing_var.locations
+        z = self.inducing_variables.locations
 
-        var_kern = kernels.VariationalKernel(
-            kernel_fn, z, qu_scale)
+        var_kern = kernels.VariationalKernel(self.kernel_fn, z, qu_scale)
 
         def var_mean(x_):
-            kxz = kernel_fn(x_, z)
-            dev = (qu_mean - mean_fn(z))[..., None]
-            return (mean_fn(x_)[..., None]
+            kxz = self.kernel_fn(x_, z)
+            dev = (qu_mean - self.mean_fn(z))[..., None]
+            return (self.mean_fn(x_)[..., None]
                     + kxz @ jscipy.linalg.cho_solve(
                         (kzz_chol, True), dev))[..., 0]
 
-        return VariationalGaussianProcess(
-            index_points, var_mean, var_kern, jitter, inducing_var)
+        return VariationalGaussianProcess(index_points,
+                                          var_mean,
+                                          var_kern,
+                                          self.jitter,
+                                          self.inducing_variables)
