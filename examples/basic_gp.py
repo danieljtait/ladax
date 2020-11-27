0d163a5c893add41d620e605e7a02009958ef8c2
danieltait <tait.djk@gmail.com>
2020-11-27 21:11:08 +0000

    convert basic_gp.py to linen

--- examples/basic_gp.py
+++ examples/basic_gp.py
@@ -9,9 +9,9 @@ from jax import random
 from jax.tree_util import tree_flatten, tree_unflatten
 import jax.numpy as jnp
 
-from typing import Callable, Tuple
-from flax import nn
-
+from typing import Callable, Tuple, Dict, Union
+from flax import linen as nn
+from flax import optim
 import scipy as oscipy
 
 from ladax import kernels, distributions, gaussian_processes, utils
@@ -22,10 +22,23 @@ flags.DEFINE_bool(
     'plot', default=False,
     help=('Plot the results.', ))
 
+flags.DEFINE_integer(
+    'n_index_points', default=25,
+    help=('Number of index points to generate. ', ))
+
+flags.DEFINE_float(
+    'learning_rate', default=0.001,
+    help=('The learning rate for the momentum optimizer.', ))
+
+flags.DEFINE_integer(
+    'num_epochs', default=100,
+    help=('Number of training epochs.', ))
+
 
 class MarginalObservationModel(nn.Module):
     """ The observation model p(y|x, {hyper par}) = ∫p(y,f|x)df where f(x) ~ GP(m(x), k(x, x')). """
-    def apply(self, pf: distributions.MultivariateNormalTriL) -> distributions.MultivariateNormalFull:
+    @nn.compact
+    def __call__(self, pf: distributions.MultivariateNormalTriL) -> distributions.MultivariateNormalFull:
         """ Applys the marginal observation model of the conditional
         Args:
             pf: distribution of the latent GP to be marginalised over,
@@ -36,23 +49,23 @@ class MarginalObservationModel(nn.Module):
         """
         obs_noise_scale = jax.nn.softplus(
             self.param('observation_noise_scale',
-                       (), jax.nn.initializers.ones))
+                       jax.nn.initializers.ones, ()))
 
         covariance = pf.scale @ pf.scale.T
         covariance = utils.diag_shift(covariance, obs_noise_scale**2)
 
-        return distributions.MultivariateNormalFull(
-            pf.mean, covariance)
+        return distributions.MultivariateNormalFull(pf.mean, covariance)
 
 
 class GaussianProcessLayer(nn.Module):
     """ Provides a Gaussian process.
     """
-    def apply(self,
-              index_points: jnp.ndarray,
-              kernel_fn: Callable,
-              mean_fn: Callable = None,
-              jitter: float =1e-4):
+    kernel_fn: Callable
+    mean_fn: Union[Callable, None] = None
+    jitter: float = 1e-4
+
+    @nn.compact
+    def __call__(self, index_points: jnp.ndarray):
         """
         Args:
             index_points: the nd-array of index points of the GP model
@@ -64,17 +77,20 @@ class GaussianProcessLayer(nn.Module):
         Returns:
             p: `distributions.MultivariateNormalTriL` object.
         """
-        if mean_fn is None:
+        if self.mean_fn is None:
             mean_fn = lambda x: jnp.zeros(x.shape[:-1], dtype=index_points.dtype)
+        else:
+            mean_fn = self.mean_fn
 
-        return gaussian_processes.GaussianProcess(index_points, mean_fn, kernel_fn, jitter)
+        return gaussian_processes.GaussianProcess(index_points, mean_fn, self.kernel_fn, self.jitter)
 
 
 class GPModel(nn.Module):
     """ Model for i.i.d noise observations from a GP with
     RBF kernel. """
 
-    def apply(self, x, dtype=jnp.float64) -> distributions.MultivariateNormalFull:
+    @nn.compact
+    def __call__(self, x: jnp.ndarray) -> distributions.MultivariateNormalFull:
         """
         Args:
             x: the nd-array of index points of the GP model.
@@ -82,33 +98,11 @@ class GPModel(nn.Module):
         Returns:
             py_x: Distribution of the observations at the index points.
         """
-        kern_fn = kernels.RBFKernelProvider(x, name='kernel_fn')
-        mean_fn = lambda x: nn.Dense(x, features=1, name='linear_mean_fn')[..., 0]
-        gp_x = GaussianProcessLayer(x, kern_fn, mean_fn, name='gp_layer')
-        py_x = MarginalObservationModel(gp_x.marginal(), name='observation_model')
+        kern_fn = kernels.RBFKernelProvider(name='kernel_fn')(x)
+        mean_fn = lambda x: nn.Dense(features=1, name='linear_mean_fn')(x)[..., 0]
+        gp_x = GaussianProcessLayer(kern_fn, mean_fn, name='gp_layer')(x)
+        py_x = MarginalObservationModel(name='observation_model')(gp_x.marginal())
         return py_x
-
-
-def build_par_pack_and_unpack(model):
-    """ Build utility functions to pack and unpack paramater pytrees
-    for the scipy optimizers. """
-    value_flat, value_tree = tree_flatten(model.params)
-    section_shapes = [item.shape for item in value_flat]
-    section_sizes = jnp.cumsum(jnp.array([item.size for item in value_flat]))
-
-    def par_from_array(arr):
-        value_flat = jnp.split(arr, section_sizes)
-        value_flat = [x.reshape(s)
-                      for x, s in zip(value_flat, section_shapes)]
-
-        params = tree_unflatten(value_tree, value_flat)
-        return params
-
-    def array_from_par(params):
-        value_flat, value_tree = tree_flatten(params)
-        return jnp.concatenate([item.ravel() for item in value_flat])
-
-    return par_from_array, array_from_par
 
 
 def get_datasets(sim_key: random.PRNGKey, true_obs_noise_scale: float = 0.5) -> Tuple[dict, dict]:
@@ -126,72 +120,88 @@ def get_datasets(sim_key: random.PRNGKey, true_obs_noise_scale: float = 0.5) -> 
     return train_ds, test_ds
 
 
-def train(train_ds):
+def get_initial_params(key):
+    init_batch = jnp.ones((FLAGS.n_index_points, 1))
+    initial_variables = GPModel().init(key, init_batch)
+    return initial_variables["params"]
+
+
+def create_optimizer(params):
+    optimizer_def = optim.Momentum(learning_rate=FLAGS.learning_rate)
+    optimizer = optimizer_def.create(params)
+    return optimizer
+
+
+@jax.jit
+def train_step(optimizer, batch):
+    """Train for a single step. """
+
+    def loss_fn(params: dict) -> float:
+        """ This is clumsier than the usual FLAX loss_fn. """
+        py = GPModel().apply({'params': params}, batch['index_points'])
+        return -py.log_prob(batch['y'])
+
+    grad_fn = jax.value_and_grad(loss_fn, has_aux=False)
+    loss, grad = grad_fn(optimizer.target)
+    optimizer = optimizer.apply_gradient(grad)
+    metrics = {'loss': loss}
+    return optimizer, metrics
+
+
+def train_epoch(optimizer, train_ds, epoch):
+    """ Train for a single epoch. """
+    optimizer, epoch_metrics = train_step(optimizer, train_ds)
+    epoch_metrics_np = jax.device_get(epoch_metrics)
+
+    logging.info('train epoch: %d, loss: %.4f',
+                 epoch,
+                 epoch_metrics_np['loss'])
+
+    return optimizer, epoch_metrics_np
+
+
+def train(train_ds) -> Dict:
     """ Complete training of the GP-Model.
     Args:
         train_ds: Python `dict` with entries `index_points` and `y`.
     Returns:
-        trained_model: A `GPModel` instance with trained hyper-parameters.
+        trained_params: The parameters of the trained model.
     """
     rng = random.PRNGKey(0)
 
-    # initialise the model
-    py, params = GPModel.init(rng, train_ds['index_points'])
-    model = nn.Model(GPModel, params)
+    # initialise the model parameters
+    params = get_initial_params(rng)
 
-    # utility functions for packing and unpacking param dicts
-    par_from_array, array_from_par = build_par_pack_and_unpack(model)
+    # get the optimizer
+    optimizer = create_optimizer(params)
 
-    @jax.jit
-    def loss_fun(model: GPModel, params: dict) -> float:
-        """ This is clumsier than the usual FLAX loss_fn. """
-        py = model.module.call(params, train_ds['index_points'])
-        return -py.log_prob(train_ds['y'])
+    for epoch in range(1, FLAGS.num_epochs + 1):
+        optimizer, metrics = train_epoch(optimizer, train_ds, epoch)
 
-    # wrap loss fun for scipy.optimize
-    def wrapped_loss_fun(arr):
-        params = par_from_array(arr)
-        return loss_fun(model, params)
-
-    @jax.jit
-    def loss_and_grads(x):
-        return jax.value_and_grad(wrapped_loss_fun)(x)
-
-    res = oscipy.optimize.minimize(
-        loss_and_grads,
-        x0=array_from_par(params),
-        jac=True,
-        method='BFGS')
-
-    logging.info('Optimisation message: {}'.format(res.message))
-
-    trained_model = model.replace(params=par_from_array(res.x))
-    return trained_model
+    return optimizer.target
 
 
 def main(_):
     train_ds, test_ds = get_datasets(random.PRNGKey(123))
-    trained_model = train(train_ds)
+    trained_params = train(train_ds)
 
     if FLAGS.plot:
         import matplotlib.pyplot as plt
 
-        obs_noise_scale = jax.nn.softplus(
-            trained_model.params['observation_model']['observation_noise_scale'])
+        obs_noise_scale = jax.nn.softplus(trained_params['observation_model']['observation_noise_scale'])
 
         def learned_kernel_fn(x1, x2):
-            return kernels.RBFKernelProvider.call(
-                trained_model.params['kernel_fn'], x1)(x1, x2)
+            return kernels.RBFKernelProvider().apply({"params": trained_params['kernel_fn']}, x1)(x1, x2)
 
         def learned_mean_fn(x):
-            return nn.Dense.call(trained_model.params['linear_mean_fn'], x, features=1)[:, 0]
+            return nn.Dense(features=1).apply({"params": trained_params['linear_mean_fn']}, x)[:, 0]
 
         # prior GP model at learned model parameters
         fitted_gp = gaussian_processes.GaussianProcess(
             train_ds['index_points'],
             learned_mean_fn,
-            learned_kernel_fn, 1e-4
-        )
+            learned_kernel_fn, 1e-4)
+
         posterior_gp = fitted_gp.posterior_gp(
                 train_ds['y'],
                 test_ds['index_points'],
